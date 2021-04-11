3f607e48c3ffe78cbed5b90d61074d689bd93fef
Alain Moratalla <realmoratalla@gmail.com>
2021-04-12 07:37:59 +0800

    added rpsls in desktop view, to add rpsls mobile and hard mode, and addtl animations during picking and displaying results

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 import aiAnswer from "./hardmode.js";
 let MODE = 'normal';
+let GAME = "rps";
 function init(){
-    console.log(MODE);
-    createStartEvents();
-    updateScore(null, MODE)
+    // console.log('%c [INIT] - to create start event and update score', "color: yellow; background-color: red;")
+    createStartEvents(GAME);
+    updateScore(null, MODE, GAME)
 }
 
 
-function createStartEvents(){
+function createStartEvents(g){
+    // console.log('%c [CREATE START EVENTS] - select all ctrl buttons and add event listeners w/ Start Game function', "color: blue; background-color: green; padding: 1em;")
     document.querySelectorAll('div.main>.controls button').forEach((controlButton,idx )=> {
-        console.log('created event listener', idx)
-        controlButton.addEventListener('click', startGame.bind(this))
+        controlButton.addEventListener('click', e => startGame(g, e))
     }) 
 }
 
 let USER_OPTION;
 
-async function startGame(e) {
-    console.log(`${MODE}, mode`, MODE)
+async function startGame(g, e) {
+    // console.log('%c [START GAME] - U & C choice then render picking results section', "color: blue; background-color: green; padding: 1em;")
     let m = MODE;
 
     document.querySelector('#mode-toggle').disabled = true;
@@ -25,21 +26,46 @@ async function startGame(e) {
     // Store the user choice in a variable
     const [ userOption ] = e.target.dataset.option.toString().split("-");
     USER_OPTION = userOption;
-    const opts = ['rock', 'paper', 'scissors']
+
+    // List computer options
+    const opts = g === 'rps'? ['paper', 'scissors', 'rock'] : ['scissors', 'spock', 'paper', 'lizard', 'rock'];
     let uOption;
-    if (userOption == 'rock') {
-        uOption = 1;
-    } else if (userOption == 'paper'){
-        uOption = 2;
+    
+    // Normalize data to pass to ai hard mode
+    if(g === "rps"){
+        // Normalize rps:[1,2,3]
+        if (userOption == 'rock') {
+            uOption = 1;
+        } else if (userOption == 'paper'){
+            uOption = 2;
+        } else {
+            uOption = 3;
+        }
     } else {
-        uOption = 3;
+        // To be replaced with corresponding values for rpsls ai hard mode
+        uOption = 1;
     }
-
-    let computerOption;
-
-    if(m === 'normal'){
-        // Computer choose an action
-        computerOption = opts[Math.floor(Math.random() * 2)]
+    /// Develop hard mode for rpsls to be passed as computer option
+    // no logic yet in hard mode for rpsls
+    // if(g === "rpsls"){
+    //     if (userOption == 'rock') {
+    //         uOption = 1;
+    //     } else if (userOption == 'paper'){
+    //         uOption = 2;
+    //     } else {
+    //         uOption = 3;
+    //     }
+    // }
+    
+    let computerOption, max, min;
+    // Computer choose an action
+    if(m === 'normal' && g === "rps"){
+        // If normal mode, just random answer
+        max = 2, min = 0;
+        computerOption = opts[Math.floor(Math.random() * (max - min + 1) + min)];
+    } else if (m === "normal" && g === "rpsls"){
+        max = 4, min = 0;
+        computerOption = opts[Math.floor(Math.random() * (max - min + 1) + min)];
     } else {
         // hard mode
     
@@ -54,22 +80,17 @@ async function startGame(e) {
             
     }
 
-    
-
     /// Proceed to picking and rendering result
     // proceed to picking
-    pickingResultsSection(userOption, computerOption, m)
-
-    // Restart the game
-    
+    await pickingResultsSection(userOption, computerOption, m, g)    
 }
 
 
 
-function pickingResultsSection(u, c, m){
+function pickingResultsSection(u, c, m, g){
 
+    // console.log('%c [PICKING RESULTS SECTION] - Render choices, update picking state, show results and update score', "color: blue; background-color: green; padding: 1em;")
     // Render results section with picking
-    console.log(document.querySelector('.controls'))
     document.querySelector('.controls').remove();
 
     // render user choice
@@ -125,30 +146,66 @@ function pickingResultsSection(u, c, m){
     let GAME_RESULT;
     let GAME_ERROR = '';
     let user = u, computer = c;
-    if(user === "rock" && computer === "scissors" || user === "paper" && computer === "rock" || user === "scissors" && computer === "paper"){
-        GAME_RESULT = 'win';
-    } else if(user === "rock" && computer === "paper" || user === "paper" && computer === "scissors" || user === "scissors" && computer === "rock"){
-        GAME_RESULT = 'lose';
-    } else if(user === computer){
-        GAME_RESULT = 'draw';
-    } else {
-        GAME_ERROR = new Error('Something went wrong with the game master');
+    if(g === 'rps'){
+        if(user === "rock" && computer === "scissors" || user === "paper" && computer === "rock" || user === "scissors" && computer === "paper"){
+            GAME_RESULT = 'win';
+        } else if(user === "rock" && computer === "paper" || user === "paper" && computer === "scissors" || user === "scissors" && computer === "rock"){
+            GAME_RESULT = 'lose';
+        } else if(user === computer){
+            GAME_RESULT = 'draw';
+        } else {
+            GAME_ERROR = new Error('Something went wrong with the game master');
+        }
+    } 
+
+    if (g === 'rpsls'){
+        if (
+			(user === "rock" && computer === "scissors") ||
+			(user === "paper" && computer === "rock") ||
+			(user === "scissors" && computer === "paper") ||
+			(user === "rock" && computer === "lizard") ||
+			(user === "lizard" && computer === "spock") ||
+			(user === "spock" && computer === "scissors") ||
+			(user === "scissors" && computer === "lizard") ||
+			(user === "paper" && computer === "spock") ||
+			(user === "lizard" && computer === "paper") ||
+			(user === "spock" && computer === "rock")
+		) {
+			GAME_RESULT = "win";
+		} else if (
+			(user === "rock" && computer === "paper") ||
+			(user === "paper" && computer === "scissors") ||
+			(user === "scissors" && computer === "rock") ||
+			(user === "lizard" && computer === "rock") ||
+			(user === "spock" && computer === "lizard") ||
+			(user === "scissors" && computer === "spock") ||
+			(user === "lizard" && computer === "scissors") ||
+			(user === "spock" && computer === "paper") ||
+			(user === "paper" && computer === "lizard") ||
+			(user === "rock" && computer === "spock") 
+
+		) {
+			GAME_RESULT = "lose";
+		} else if (user === computer) {
+			GAME_RESULT = "draw";
+		} else {
+			GAME_ERROR = new Error("Something went wrong with the game master");
+		}
     }
+    
     let result = GAME_RESULT;
-    console.log(result)
 
     // render 
     
     const resultsPaneDiv = document.createElement('div');
     resultsPaneDiv.classList = "results-pane";
     const resultBannerHeadings2 = document.createElement('h2');
-    console.log(resultBannerHeadings2)
     resultBannerHeadings2.classList = "result-banner";
     resultBannerHeadings2.textContent = result.toString().toLowerCase().trim() !== "draw" ? `YOU ${result.toString().toUpperCase().trim()}`: "IT'S A TIE";
     const resetButton = document.createElement('button');
     resetButton.id = "reset";
     resetButton.textContent = "PLAY AGAIN";
-    resetButton.addEventListener('click', resetGame)
+    resetButton.addEventListener('click', resetGame.bind(this, g))
 
     resultsPaneDiv.prepend(resultBannerHeadings2);
     resultsPaneDiv.append(resetButton);
@@ -158,71 +215,43 @@ function pickingResultsSection(u, c, m){
         
         userOptionDiv.insertAdjacentElement('afterend', resultsPaneDiv)
         if (result === 'win'){
-            updateScore('add', m, result)
+            updateScore('add', m, g, result)
         } else {
-            console.log('You loseee', m, result)
-            updateScore('add', m, result)
+            updateScore('add', m, g, result)
         } 
     }, 3000);
     
 }
 
 
-function resetGame() {
+function resetGame(g) {
+    // console.log('%c [RESET GAME] - Remove result section, render controls, initialize again', "color: blue; background-color: green; padding: 1em;")
     document.querySelector('.results-section').remove();
     document.querySelector('#mode-toggle').disabled = false;
-
     const controlDiv = document.createElement('div');
-    const paperButton = document.createElement('button');
-    const scissorsButton = document.createElement('button');
-    const rockButton = document.createElement('button');
-    const paperOptionSpan = document.createElement('span');
-    const scissorsOptionSpan = document.createElement('span');
-    const rockOptionSpan = document.createElement('span');
-    const paperOptionImage = document.createElement('img');
-    const scissorsOptionImage = document.createElement('img');
-    const rockOptionImage = document.createElement('img');
+    let ctrlList;
 
-    controlDiv.classList = 'controls'
-
-    paperButton.classList = "paper-option paper__option__main"
-    paperButton.dataset.option = "paper-option"
-    paperOptionSpan.dataset.option = "paper-option"
-    paperOptionImage.dataset.option = "paper-option"
-    paperOptionImage.src = "./images/icon-paper.svg"
-    paperOptionImage.alt = "paper"
-
-    scissorsButton.classList = "scissors-option scissors__option__main"
-    scissorsButton.dataset.option = "scissors-option"
-    scissorsOptionSpan.dataset.option = "scissors-option"
-    scissorsOptionImage.dataset.option = "scissors-option"
-    scissorsOptionImage.src = "./images/icon-scissors.svg"
-    scissorsOptionImage.alt = "scissors"
-
-    rockButton.classList = "rock-option rock__option__main"
-    rockButton.dataset.option = "rock-option"
-    rockOptionSpan.dataset.option = "rock-option"
-    rockOptionImage.dataset.option = "rock-option"
-    rockOptionImage.src = "./images/icon-rock.svg"
-    rockOptionImage.alt = "rock"
-    rockOptionImage.classList = "rock-image"
-
+    if(g === 'rps'){
+        ctrlList = renderControls(g);
+        controlDiv.classList = 'controls controls__rps';
+        ctrlList.forEach(ctrl => {
+            controlDiv.append(ctrl);
+        })  
+    } 
+    if(g === 'rpsls'){
+        ctrlList = renderControls(g);
+        controlDiv.classList = "controls controls__rpsls";
+        ctrlList.forEach(ctrl => {
+            controlDiv.append(ctrl);
+        })
+    }
+        
     document.querySelector('div.main').prepend(controlDiv);
-    controlDiv.prepend(paperButton);
-    controlDiv.append(scissorsButton);
-    controlDiv.append(rockButton);
-    paperButton.prepend(paperOptionSpan);
-    paperOptionSpan.prepend(paperOptionImage);
-    scissorsButton.prepend(scissorsOptionSpan);
-    scissorsOptionSpan.prepend(scissorsOptionImage);
-    rockButton.prepend(rockOptionSpan);
-    rockOptionSpan.prepend(rockOptionImage);
-
     init();
 }
 
 function toggleModal(e){
-    if(e.target.className === 'close'){
+    if(e.target.classList[0] === 'close'){
         let body = document.querySelector('body')
         let pageWrap = document.createElement('div');
         pageWrap.classList = "page-wrap";
@@ -237,7 +266,7 @@ function toggleModal(e){
         closeButtonImage.alt ="Close button to close the rules";
         let rpsRulesImage = document.createElement('img');
         rpsRulesImage.classList ="img__rules";
-        rpsRulesImage.src ="./images/image-rules.svg";
+        rpsRulesImage.src = GAME == "rps" ? "./images/image-rules.svg": "./images/image-rules-bonus.svg"
         rpsRulesImage.alt="Here are the rules: Paper beats rock, Rock beats scissors, Scissors beat paper"
 
         modalDiv.append(rulesHeadings3);
@@ -288,7 +317,7 @@ document.querySelector('#mode-toggle').addEventListener('click', (e) => {
         aiScoreDiv.append(aiScoreSpan)
         aiScoreDiv.append(aiScoreP);
         scoreHeadDiv.append(aiScoreDiv);
-        updateScore(null, MODE)
+        updateScore(null, MODE, GAME)
 
         // scoreCardUpdate('hard')
     } else {
@@ -301,7 +330,7 @@ document.querySelector('#mode-toggle').addEventListener('click', (e) => {
         document.querySelector('.ai-score').remove()
         document.querySelector('.score h4').textContent = 'SCORE'
 
-        updateScore(null, MODE)
+        updateScore(null, MODE, GAME)
 
        
     }
@@ -310,36 +339,47 @@ document.querySelector('#mode-toggle').addEventListener('click', (e) => {
 
 init();
 
-function updateScore(op, m, r,prev, curr){
+function updateScore(op, m, g, r, prev, curr){
     prev = parseInt(document.querySelector('#score').textContent)
-    if(!op){
-        console.log('triggered no operation')
+    if(!op && g === "rps"){
         if(m !== "hard"){
-            console.log('triggered no operation normal')
             prev = window.localStorage.getItem('rps-score');
-            console.log(prev)
             document.querySelector('#score').textContent = prev ? prev: '0';
         } else {
-            console.log('triggered no operation hard')
             prev = window.localStorage.getItem('rps-hard-score');
             document.querySelector('#score').textContent = prev ? prev.split(",")[0]: '0';
             document.querySelector('#ai-score').textContent = prev ? prev.split(",")[1]: '0';
-            console.log(document.querySelector('#ai-score'))
         }
-        
     }
 
-    console.log(op, m, r)
+    if(!op && g === "rpsls"){
+        if(m !== "hard"){
+            prev = window.localStorage.getItem('rpsls-score');
+            document.querySelector('#score').textContent = prev ? prev: '0';
+        } else {
+            prev = window.localStorage.getItem('rpsls-hard-score');
+            document.querySelector('#score').textContent = prev ? prev.split(",")[0]: '0';
+            document.querySelector('#ai-score').textContent = prev ? prev.split(",")[1]: '0';
+        }
+    }
 
-    if(op === 'add' && r === "win" && m !== "hard"){
-        console.log('TRIGGERED update score normal mode')
+
+    if(op === 'add' && r === "win" && m !== "hard" && g === "rps"){
         document.querySelector('#score').textContent = parseInt(document.querySelector('#score').textContent) + 1;
         curr = document.querySelector('#score').textContent
         window.localStorage.setItem('rps-score', curr)
     }
+
+    if(op === 'add' && r === "win" && m !== "hard" && g === "rpsls"){
+        document.querySelector('#score').textContent = parseInt(document.querySelector('#score').textContent) + 1;
+        curr = document.querySelector('#score').textContent
+        window.localStorage.setItem('rpsls-score', curr)
+    }
+
+
+
     
-    if(op === "add" && r === "win" && m === "hard"){
-        console.log('TRIGGERED update score hard mode')
+    if(op === "add" && r === "win" && m === "hard" && g === "rps"){
         document.querySelector('#score').textContent = parseInt(document.querySelector('#score').textContent) + 1;
         curr = ['', '']
         curr[0] = document.querySelector('#score').textContent;
@@ -348,8 +388,7 @@ function updateScore(op, m, r,prev, curr){
     }
 
 
-    if(op === "add" && r === "lose" && m === "hard"){
-        console.log('TRIGGERED update score hard mode')
+    if(op === "add" && r === "lose" && m === "hard" && g === "rps"){
         document.querySelector('#ai-score').textContent = parseInt(document.querySelector('#ai-score').textContent) + 1;
         curr = ['', '']
         curr[0] = document.querySelector('#score').textContent;
@@ -357,13 +396,107 @@ function updateScore(op, m, r,prev, curr){
         window.localStorage.setItem('rps-hard-score', curr)
     }
 
+    if(op === "add" && r === "win" && m === "hard" && g === "rpsls"){
+        document.querySelector('#score').textContent = parseInt(document.querySelector('#score').textContent) + 1;
+        curr = ['', '']
+        curr[0] = document.querySelector('#score').textContent;
+        curr[1] = document.querySelector('#ai-score').textContent;
+        window.localStorage.setItem('rpsls-hard-score', curr)
+    }
+
+
+    if(op === "add" && r === "lose" && m === "hard" && g === "rpsls"){
+        document.querySelector('#ai-score').textContent = parseInt(document.querySelector('#ai-score').textContent) + 1;
+        curr = ['', '']
+        curr[0] = document.querySelector('#score').textContent;
+        curr[1] = document.querySelector('#ai-score').textContent;
+        window.localStorage.setItem('rpsls-hard-score', curr)
+    }
+
+
     return { op, prev, curr }
 }
 
-function scoreCardUpdate(m){
-    if (m === 'hard'){
-        document.querySelector('#score').textContent = "0";
-        document.querySelector('#ai-score').textContent = "0";
+
+
+document.querySelector('#game-toggle').addEventListener('click', (e) => {
+    const header = document.querySelector('header>div.head__header');
+    let logo = header.querySelector('img');
+    const main = document.querySelector('main>div.main');
+    const controls = main.querySelector('div.controls')
+
+    if(GAME === 'rps'){
+        // Change to rpsls
+        logo.class = "logo__rpsls";
+        logo.src = "./images/logo-bonus.svg";
+        main.classList = "main main__rpsls";
+        controls.remove();
+        let newControls = document.createElement('div');
+
+        newControls.classList = "controls controls__rpsls";
+        main.append(newControls);
+        
+        // create rpsls controls
+        const controlList = renderControls('rpsls');
+        
+        controlList.forEach(ctrl => {
+            newControls.append(ctrl);
+        })
+
+        e.target.textContent = 'RPSLS'
+        e.target.style.color = 'hsl(237, 49%, 15%)';
+        e.target.style['background-position']= "100%";
+        GAME = 'rpsls'
+        updateScore(null, MODE, GAME)
+
+    } else { 
+        // Change to rps
+        logo.class = "logo__rps";
+        logo.src = "./images/logo.svg";
+        main.classList = "main main__rps";
+        controls.remove();
+        let newControls = document.createElement('div');
+
+        newControls.classList = "controls controls__rps";
+        main.append(newControls);
+
+        // create rps controls
+        const controlList = renderControls('rps');
+        controlList.forEach(ctrl => {
+            newControls.append(ctrl);
+        })
+
+
+        e.target.textContent = 'RPS';
+        e.target.style.color = 'white';
+        e.target.style['background-position']= "0%";
+        GAME = 'rps'
+        
+        updateScore(null, MODE, GAME)
     }
 
+    createStartEvents(GAME)
+})
+
+function renderControls(g) {
+    let opts = g === 'rps'? ['paper', 'scissors', 'rock'] : ['scissors', 'spock', 'paper', 'lizard', 'rock'];
+    let controls = [];
+    opts.forEach(opt => {
+        let a = document.createElement('button'),
+        b = document.createElement('span'),
+        c = document.createElement('img');
+        a.dataset.option = `${opt}-option`;
+        b.dataset.option = `${opt}-option`;
+        c.dataset.option = `${opt}-option`;
+        a.classList = `${opt}-option ${opt}__option__main`;
+        // a.addEventListener('click', e => startGame(g,e));
+        c.src = `./images/icon-${opt}.svg`;
+        c.alt = opt;
+        c.classList = `${opt}-image`
+        b.append(c);
+        a.append(b);
+        controls.push(a);
+    })
+
+    return controls;
 }
